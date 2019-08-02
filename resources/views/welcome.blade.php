ba742ac4cb00151728dea378ad0ba62768cfdc03
Alirio Aranguren <a.aranguren@concentra.com.do>
2019-08-02 12:06:02 -0400

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Alirio Aranguren
+                    Alirio Aranguren 
                 </div>
 
                 <div class="links">
