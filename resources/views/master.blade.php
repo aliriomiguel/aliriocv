47ffb083e91d0a73f02e45b38ae4510e3b9e5de5
Alirio Aranguren <a.aranguren@concentra.com.do>
2019-10-16 15:27:44 -0400

    changed dashboard styles

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,97 +16,108 @@
 </head>
 <body>
     {{-- Menu --}}
-    <div class="bg-otk text-white p-5 mb-3">
-            <nav class="navbar navbar-expand-lg bg-otk text-white">
-                <button class="navbar-toggler navbar-light bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon text-white"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item p-1">
-                            <a href="/" class="btn btn-secondary btn-sm bg-otk">Landing Page</a>
-                        </li>
-                        <li class="nav-item p-1">
-                            <a href="{{route('dashboard.index')}}" class="btn btn-secondary btn-sm bg-otk"><i class="fas fa-home"></i></a>
-                        </li>
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Posts
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('posts.index')}}">Show Posts</a>
-                            <a class="dropdown-item" href="{{route('posts.create')}}">Create Post</a>
-                            {{-- <div class="dropdown-divider"></div> --}}
-                            </div>
-                        </li>
 
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Categories
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('categories.index')}}">Show Category</a>
-                            <a class="dropdown-item" href="{{route('categories.create')}}">Create Category</a>
-                            </div>
-                        </li>
 
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            About Texts
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('abouts.index')}}">Show About Texts</a>
-                            <a class="dropdown-item" href="{{route('abouts.create')}}">Create About Text</a>
-                            </div>
-                        </li>
 
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Services
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('services.index')}}">Show Services</a>
-                            <a class="dropdown-item" href="{{route('services.create')}}">Add Service</a>
-                            </div>
-                        </li>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Alirio Resume
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Portfolio
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('portfolios.index')}}">Show Portfolio</a>
-                            <a class="dropdown-item" href="{{route('portfolios.create')}}">Add New Portfolio Entry</a>
-                            </div>
-                        </li>
+            <button class="navbar-toggler navbar-light bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon text-white"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    {{-- <li class="nav-item p-1">
+                        <a href="/" class="nav-link">Landing Page</a>
+                    </li> --}}
+                    <li class="nav-item p-1">
+                        <a href="{{route('dashboard.index')}}" class="nav-link"><i class="fas fa-home"></i></a>
+                    </li>
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Posts
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('posts.index')}}">Show Posts</a>
+                        <a class="dropdown-item" href="{{route('posts.create')}}">Create Post</a>
+                        {{-- <div class="dropdown-divider"></div> --}}
+                        </div>
+                    </li>
 
-                        <li class="nav-item p-1 dropdown">
-                            <button class="btn btn-secondary btn-sm dropdown-toggle bg-otk" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Quotes
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{route('quotes.index')}}">Show Quotes</a>
-                            <a class="dropdown-item" href="{{route('quotes.create')}}">Add Quote</a>
-                            </div>
-                        </li>
-                        <li class="nav-item p-1">
-                            <a href="{{route('contacts.index')}}" class="btn btn-secondary btn-sm bg-otk"><i class="fas fa-envelope"></i></a>
-                        </li>
-                        
-                    </ul>
-                    @auth
-                    <form class="d-inline-block float-right" action="{{route('logout')}}" method="post">
-                        @csrf
-                        <a style="font-size: 13px;">Logged as {{auth()->user()->name}}</a> | <button class="btn btn-sm btn-secondary">Logout</button>
-                    </form>
-                    @else
-                    <a href="{{route('login')}}" class="btn btn-secondary d-inline-block float-right">Login</a>
-                    @endauth
-                </div>
-            </nav>
-    </div>
-    {{-- end menu --}}
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categories
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('categories.index')}}">Show Category</a>
+                        <a class="dropdown-item" href="{{route('categories.create')}}">Create Category</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About Texts
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('abouts.index')}}">Show About Texts</a>
+                        <a class="dropdown-item" href="{{route('abouts.create')}}">Create About Text</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Services
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('services.index')}}">Show Services</a>
+                        <a class="dropdown-item" href="{{route('services.create')}}">Add Service</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Portfolio
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('portfolios.index')}}">Show Portfolio</a>
+                        <a class="dropdown-item" href="{{route('portfolios.create')}}">Add New Portfolio Entry</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item p-1 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Quotes
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('quotes.index')}}">Show Quotes</a>
+                        <a class="dropdown-item" href="{{route('quotes.create')}}">Add Quote</a>
+                        </div>
+                    </li>
+                    <li class="nav-item p-1">
+                        <a href="{{route('contacts.index')}}" class="nav-link"><i class="fas fa-envelope"></i></a>
+                    </li>
+                    
+                </ul>
+                @auth
+                <form class="d-inline-block float-right" action="{{route('logout')}}" method="post">
+                    @csrf
+                    <a style="font-size: 13px;">Logged as {{auth()->user()->name}}</a> | <button class="btn btn-sm btn-secondary">Logout</button>
+                </form>
+                @else
+                <a href="{{route('login')}}" class="btn btn-secondary d-inline-block float-right">Login</a>
+                @endauth
+            </div>
+        </div>
+    </nav>
+    <br>
+        {{-- end menu --}}
     <div class="container-fluid "> 
         <div class="main container">
                 @yield('content')
